bfa8ab28ce376a8a91839161b7af7aa6a6498559
jay177686 <jay177686@gmail.com>
2026-04-18 23:13:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.uix.label import Label
-
 
 class MyApp(App):
     def build(self):
-        self.count = 0
-
-        layout = BoxLayout(orientation='vertical')
-
-        self.label = Label(text="0", font_size=60)
-        btn = Button(text="点击 +1", font_size=40)
-
-        btn.bind(on_press=self.add)
-
-        layout.add_widget(self.label)
-        layout.add_widget(btn)
-
-        return layout
-
-    def add(self, instance):
-        self.count += 1
-        self.label.text = str(self.count)
-
+        return Label(text="Hello APK!")
 
 if __name__ == "__main__":
     MyApp().run()
